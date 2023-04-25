e21bbf3600fb6b87e55267aaae2cae7ff1542608
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-04-25 18:26:37 +0200

    start

---
+++ fifteen-puzzle-project/main.py
@@ -0,0 +1,9 @@
+
+def print_hi(name):
+    print("No to lecimy z tym zadankiem")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
