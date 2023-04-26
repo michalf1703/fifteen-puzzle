c9ba6a71bccabd87dd811f6aa99383032f7c8b06
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-04-26 15:50:26 +0200

    add przeszukiwania.ps1 and przeszukiwaniaRunner.ps1

--- fifteen-puzzle-project/main.py
+++ fifteen-puzzle-project/main.py
@@ -2,6 +2,9 @@ import numpy as np
 import sys
 import board
 import dfs
+import fileOperations as read
+import fileOperations as save
+import time
 
 
 
@@ -14,3 +17,15 @@ if len(sys.argv) > 1:
     endFile = sys.argv[4]
     additionalStats = sys.argv[5]
 
+board_tab = read.read_file(startFile)
+board_width = read.get_width(startFile)
+board_height = read.get_height(startFile)
+board = board.Board(board_width,board_height,board_tab)
+
+if algorithm == "dfs":
+    solver = dfs.dfs()
+    solvingStartTime = time.time_ns()
+    solutionSequence = solver.solve(board,21,"","", parameter)
+    solvingTime = time.time_ns() - solvingStartTime
+    solvingTime = solvingTime / 1000000
+    save.save_to_file(solver.found, endFile, additionalStats, solver.solution, solver.visitedStates, solver.processedStates.__len__(), solver.reachedDepth, solvingTime)
