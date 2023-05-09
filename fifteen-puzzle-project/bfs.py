a06af1da2c171be96f1d2f5f383fc021061fb5ae
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-05-09 03:35:54 +0200

    update function change_state()

--- fifteen-puzzle-project/bfs.py
+++ fifteen-puzzle-project/bfs.py
@@ -4,12 +4,11 @@ from collections import deque
 class bfs:
     def __init__(self, board):
         self.board = board
-        self.visited = set()                            # zbiór odwiedzonych stanów
-        self.visited_states = 1                         # liczba odwiedzonych stanów
-        self.processed_states = 0                       # liczba przetworzonych stanów
-        self.time = 0                                   # czas wykonania algorytmu
-        self.max_depth_reached = 0                      # maksymalna głębokość, na którą zszedł algorytm
-
+        self.visited = set()
+        self.visited_states = 1
+        self.processed_states = 0
+        self.elapsed_time = 0
+        self.max_recursion_reached = 0
 
     def bfs_solve(self):
         star_time = time.time_ns()
@@ -32,15 +31,14 @@ class bfs:
         self.elapsed_time = (time.time_ns() - star_time) / (10 ** 6)
         return None
 
-
     # get_states_count --> Zwraca liczbę odwiedzonych i przetworzonych stanów podczas działania algorytmu.
     def get_states_count(self):
         return self.visited_states, self.processed_states
 
     #get_time -->Zwraca czas wykonania algorytmu w sekundach, z dokładnością do 3 miejsc po przecinku
     def get_time(self):
-        return round(self.time, 3)
+        return round(self.elapsed_time, 3)
 
     #get_max_depth_reached --> Zwraca maksymalną rekursję, jaką osiągnął algorytm
     def get_max_depth_reached(self):
-        return self.max_depth_reached
+        return self.max_recursion_reached
